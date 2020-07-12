65022efe2dc2cf4364c88cf104fa4ed1672fce47
ajivinister <ajivinister@gmail.com>
2020-07-12 15:44:00 +0530

    Affliation added.

---
+++ Pages/Client/Affliation.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MM.ClientModels;
+
+namespace MM.Pages.Client
+{
+    public class AffliationModel : PageModel
+    {
+        private readonly ClientDbContext _context;
+
+        public AffliationModel(ClientDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public IList<Affliation> AffliationList { get;set; }
+
+        [BindProperty]
+        public Affliation Affliation { get; set; }
+
+        public async Task<IActionResult> OnGetListAsync()
+        {
+            return new JsonResult(await _context.Affliation.ToListAsync());
+        }
+
+        public async Task<IActionResult>  OnGetSelectedRecordAsync(int id)
+        {
+            return new JsonResult(await _context.Affliation.Where(x=>x.Id==id).FirstOrDefaultAsync());
+        }
+    
+ 
+        public async Task<IActionResult> OnPostSaveAsync(Affliation Affliation)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return new JsonResult(new { success = false, message = "Error. Please check values entered" });
+            }
+
+            if (Affliation.Id > 0)
+            {
+                _context.Attach(Affliation).State = EntityState.Modified;
+            }
+            else
+            {
+                _context.Affliation.Add(Affliation);
+            }
+             await _context.SaveChangesAsync();
+            return new JsonResult( new { success = true, message = "Saved successfully" });
+        }
+
+         public async Task<IActionResult> OnGetDeleteAsync(int? id)
+        {
+
+            if (id == null)
+            {
+                return new JsonResult(new { success = false, message = "No such reccord found to delete" });
+            }
+
+            Affliation = await _context.Affliation.FindAsync(id);
+
+            if (Affliation != null)
+            {
+                _context.Affliation.Remove(Affliation);
+                await _context.SaveChangesAsync();
+                return new JsonResult(new { success = true, message = "Deleted successfully" });
+            }
+            return new JsonResult(new { success = false, message = "No such reccord found to delete" });
+
+        }
+    }
+}
