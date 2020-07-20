07b23070b52452e84962a99ba1b28943d3f1165d
shatlin <shatlin@github.com>
2020-07-20 17:08:45 +0200

    Memberloginscreens

---
+++ Pages/Member/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MM.Pages.Member.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
